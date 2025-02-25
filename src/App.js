995ef26c0f15bc4aee35b0bff230a7a25eb1fcfc
Jakob Eriksson <jkberiksson@gmail.com>
2025-02-25 17:24:57 +0100

    Fix indentation and adjust height for main content in App component

--- src/App.js
+++ src/App.js
@@ -6,30 +6,30 @@ import { useEffect, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 
 function App() {
-  const [showLoader, setShowLoader] = useState(true);
+    const [showLoader, setShowLoader] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setShowLoader(false);
-    }, 4000);
-  }, []);
-  return (
-    <AnimatePresence mode='wait'>
-      {showLoader ? (
-        <div className='overflow-hidden'>
-          <Loader key='loader' />
-        </div>
-      ) : (
-        <motion.div
-          key='main-content'
-          className='max-w-[1600px] mx-auto h-screen px-6 flex flex-col justify-between overflow-hidden'>
-          <Header />
-          <Home />
-          <Footer />
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
+    useEffect(() => {
+        setTimeout(() => {
+            setShowLoader(false);
+        }, 4000);
+    }, []);
+    return (
+        <AnimatePresence mode='wait'>
+            {showLoader ? (
+                <div className='overflow-hidden'>
+                    <Loader key='loader' />
+                </div>
+            ) : (
+                <motion.div
+                    key='main-content'
+                    className='max-w-[1600px] mx-auto h-[100dvh] px-6 flex flex-col justify-between overflow-hidden'>
+                    <Header />
+                    <Home />
+                    <Footer />
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
 }
 
 export default App;
